cee9d716ef31e04ff7f54a26d98d75c7cb2eb88d
nmowens95 <nmowens95@gmail.com>
2023-08-25 13:45:20 -0500

    Fixed previous code

--- graphics.py
+++ graphics.py
@@ -1,13 +1,14 @@
 from tkinter import Tk, BOTH, Canvas
 
-class Window():
+
+class Window:
     def __init__(self, width, height):
         self.root = Tk()
         self.root.title("Maze Solver")
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root, bg="white", height=height, width=width)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.root.update_idletasks()
@@ -19,7 +20,30 @@ class Window():
             self.redraw()
         print("window closed...")
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
+
     def close(self):
         self.running = False
 
-Window()
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
