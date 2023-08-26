1dd277a5c788272c00ce1d8140b747a4114d7ef9
Nathan Owens <nmowens95@gmail.com>
2023-08-26 18:49:32 -0500

    Needed to fix some variable names

--- cell.py
+++ cell.py
@@ -20,13 +20,13 @@ class Cell:
         self.y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
-            self._win.draw_line(line)
+            self.win.draw_line(line)
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(line)
+            self.win.draw_line(line)
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
-            self._win.draw_line(line)
+            self.win.draw_line(line)
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line)
+            self.win.draw_line(line)
