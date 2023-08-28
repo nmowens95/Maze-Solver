d8819d5607182832657ef4119b2a8f91b4631397
nmowens95 <nmowens95@gmail.com>
2023-08-28 14:32:10 -0500

    Added lines to draw from different point

--- cell.py
+++ cell.py
@@ -30,3 +30,45 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        if self.win is None:
+            return
+        x_mid = (self.x1 + self.x2) / 2
+        y_mid = (self.y1 + self.y2) / 2
+        # we have our mid point, now we are going to initialize the cells we are drawing to
+        to_x_mid = (to_cell.x1 + to_cell.x2)
+        to_y_mid = (to_cell.y1 + to_cell.y2)
+
+        # use the colors to trace the change
+        fill_color = "red"
+        if undo:
+            fill_color = "grey"
+
+        # move left
+        if self.x1 > to_cell.x1:
+            line = Line(Point(self.x1, y_mid), Point(x_mid, y_mid))
+            self.win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_cell.x2, to_y_mid))
+            self.win.draw_line(line, fill_color)
+
+        # moving right
+        elif self.x1 < to_cell.x1:
+            line = Line(Point(x_mid, y_mid), Point(self.x2, y_mid))
+            self.win.draw_line(line, fill_color)
+            line = Line(Point(to_cell.x1, to_y_mid), Point(to_x_mid, to_y_mid))
+            self.win.draw_line(line, fill_color)
+
+        # moving up
+        elif self.y1 > to_cell.y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self.y1))
+            self.win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_cell.y2), Point(to_x_mid, to_y_mid))
+            self.win.draw_line(line, fill_color)
+
+        # moving down
+        elif self.y1 < to_cell.y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self.y2))
+            self.win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_cell.y1))
+            self.win.draw_line(line, fill_color)
